a88031d510c15691e15cd68e2d71933f4bef7641
Birkan Uzun <birkanuzun27@gmail.com>
2015-04-12 14:56:45 -0400

    Add required libraries for the form date-time-picker

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 
-    // Click listener for "Find a Trip" button, shows search window
-    $("#btn_find_trip").click(function(e) {
-        $("#search_window").css("visibility", "visible");
-    });
+    // // Click listener for "Find a Trip" button, shows search window
+    // $("#btn_find_trip").click(function(e) {
+    //     $("#search_window").css("visibility", "visible");
+    // });
 
-    $(function () {
-        $('#datetimepicker_start').datetimepicker();
-    });    
+    // $(function () {
+    //     $('#datetimepicker_start').datetimepicker();
+    // });    
+    
 });
