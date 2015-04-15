93ce9b6d49483a490862a4a1abc6f87627f69d80
Tony Tran <tmtran7393@gmail.com>
2015-04-14 21:02:45 -0400

    center modals

--- js/interested_users.js
+++ js/interested_users.js
@@ -61,4 +61,19 @@ $(document).ready(function() {
     interval: false,
   });
 
+  // Center all modals 
+  // Source: http://www.minimit.com/articles/solutions-tutorials/vertical-center-bootstrap-3-modals
+  function centerModals(){
+    $('.modal').each(function(i){
+      var $clone = $(this).clone().css('display', 'block').appendTo('body');
+      var top = Math.round(($clone.height() - $clone.find('.modal-content').height()) / 2);
+      top = top > 0 ? top : 0;
+      $clone.remove();
+      $(this).find('.modal-content').css("margin-top", top);
+    });
+  }
+  
+  $('.modal').on('show.bs.modal', centerModals);
+  $(window).on('resize', centerModals);
+
 });
