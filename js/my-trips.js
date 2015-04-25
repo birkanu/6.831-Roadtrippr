1ffc18d1bd0906b4553064158014330cd1d1c65a
Birkan Uzun <birkanuzun27@gmail.com>
2015-04-24 20:47:48 -0400

    My Trips page fixed, shows trips from Firebase

--- js/my-trips.js
+++ js/my-trips.js
@@ -10,85 +10,91 @@ function getQueryVariable(variable) {
 }
 
 $(document).ready(function() {
-    var shared_new_trip = getQueryVariable("shared-new-trip");
-    var new_trip;
-    
-    if (shared_new_trip) {
-        new_trip = localStorage.getItem("new_trip_details");
-    }
+    var ref = new Firebase("https://shining-fire-2402.firebaseio.com");
+    var current_user;
+    var authData = ref.getAuth();
+    if (authData) {
+        ref.child("users").child(authData.uid).once('value', function(dataSnapshot) {
+            current_user = dataSnapshot.val();
+            var current_user_trip_uids = current_user.trips.split(", ");
+            current_user_trip_uids = current_user_trip_uids.reverse();
+            var search_result_source = $("#search_results").html();
+            var search_result_template = Handlebars.compile(search_result_source);
+            var search_result_context = {
+                trips: []
+            }
 
-    // Populate dummy search results
-    var search_result_source = $("#search_results").html();
-    var search_result_template = Handlebars.compile(search_result_source);
-    var search_result_context = {
-        trips: []
-    }
+            for (var i = 0; i < current_user_trip_uids.length; i++) {
+                var idx = current_user_trip_uids[i];
+                (function(cntr) {
+                    ref.child("trips").child(idx).once('value', function(dataSnapshot) {
+                        current_trip = dataSnapshot.val();
+                        var stops = [];
+                        for (var s = 0; s < current_trip.stops.length; s++) {
+                            stops.push(current_trip.stops[s].name);
+                        }
+                        idx = current_user_trip_uids[cntr];
+                        trip = {
+                            index: idx,
+                            trip_id: idx,
+                            full_route_id: "full_route_"+idx,
+                            abbr_route_id: "abbr_route_"+idx,
+                            trip_name: current_trip.name,
+                            planned_abbr_route: [stops[0]].concat(stops[1]).concat("...").concat([stops[stops.length - 1]]),
+                            planned_full_route: stops,
+                            start_date: current_trip.start_date,
+                            end_date: current_trip.end_date,
+                            duration: current_trip.duration,
+                            num_companions: current_trip.companion_count,
+                            are_dates_flexible: current_trip.are_dates_flexible,
+                            map_img_src: "http://dishaan.scripts.mit.edu/map-1.png",
+                            notes: current_trip.notes,
+                            map_alt_text: "Route map from " + current_trip.start_location + " to " + current_trip.end_location,
+                        }
+                        search_result_context.trips.push(trip);
+                        if (cntr == current_user_trip_uids.length - 1) {
+                            var search_result_source_processed = search_result_template(search_result_context);
+                            $("#search_results").html(search_result_source_processed);         
+                            var shared_new_trip = getQueryVariable("shared-new-trip");
+                            if (shared_new_trip) {
+                                $(".trip-creation").show();
+                            }
 
-    var trip_dict = {};
-    var road_trip_indices = localStorage.getItem('current_road_trip_indices').split(",");
-    
-    for (var i = 0; i < road_trip_indices.length; i++) {
-        var idx = parseInt(road_trip_indices[i]);
-        cur_trip_json = road_trips[idx];
-        trip = {
-            index: idx,
-            trip_id: "trip_" +idx,
-            full_route_id: "full_route_"+idx,
-            abbr_route_id: "abbr_route_"+idx,
-            trip_name: cur_trip_json.trip_name,
-            planned_abbr_route: [cur_trip_json.start_location].concat(cur_trip_json.stops[0]).concat("...").concat([cur_trip_json.end_location]),
-            planned_full_route: [cur_trip_json.start_location].concat(cur_trip_json.stops).concat([cur_trip_json.end_location]),
-            start_date: cur_trip_json.start_date,
-            end_date: cur_trip_json.end_date,
-            duration: cur_trip_json.duration,
-            num_companions: cur_trip_json.num_companions,
-            are_dates_flexible: cur_trip_json.are_dates_flexible,
-            map_img_src: "http://dishaan.scripts.mit.edu/map-" + idx + ".png",
-            notes: cur_trip_json.notes,
-            map_alt_text: "Route map from " + cur_trip_json.start_location + " to " + cur_trip_json.end_location,
-        }
-        search_result_context.trips.push(trip);
-        trip_dict["trip_"+idx] = trip;
-    }
+                            // for (var k = 0; k < search_result_context.trips.length; k++) {
+                            //     var idx3 = search_result_context.trips[k]["index"];;
+                            //     $("#trip_"+idx3).click(function(e) {
+                            //         alert("AM")
+                            //         // var clicked_trip = trip_dict[this.id];
+                            //         // localStorage.setItem('trip', JSON.stringify(clicked_trip));
+                            //         // this.href = "trip-details.html";
+                            //         // document.location.href = "trip-details.html";            
+                            //     });
+                            // }
 
-    var search_result_source_processed = search_result_template(search_result_context);
-    $("#search_results").html(search_result_source_processed);
-
-    if (new_trip) {
-        $(".trip-creation").show();
-    }
-
-    // TODO(Tony): Fix behavior when trip is clicked. Create a trip object as necessary to interface with Birkan's edit trip screen
-    for (var t = 0; t < road_trip_indices.length; t++) {
-        var idx = parseInt(road_trip_indices[t]);
-        $("#trip_"+idx).click(function(e) {
-            var clicked_trip = trip_dict[this.id];
-            localStorage.setItem('trip', JSON.stringify(clicked_trip));
-            this.href = "trip-details.html";
-            document.location.href = "trip-details.html";            
+                            // Add hover on abbreviated planned route for each route
+                            for (var j = 0; j < search_result_context.trips.length; j++) {
+                                var idx2 = search_result_context.trips[j]["index"];
+                                var full_route_div = $("#full_route_"+idx2);
+                                var abbr_route_div = $("#abbr_route_"+idx2);
+                                full_route_div.offset({left: full_route_div.offset().left, top: abbr_route_div.offset().top});
+                                full_route_div.toggle();
+                                abbr_route_div.mouseenter(function(e) {
+                                    e.preventDefault();
+                                    e.stopPropagation();
+                                    var full_route_div_id = this.id.replace("abbr", "full");
+                                    $("#"+full_route_div_id).fadeIn(500);
+                                });
+                                full_route_div.mouseleave(function(e) {
+                                    e.preventDefault();
+                                    e.stopPropagation();
+                                    var full_route_div_id = this.id.replace("abbr", "full");
+                                    $("#"+full_route_div_id).fadeOut(500);            
+                                });        
+                            }
+                        }
+                    });
+                })(i, idx);
+            } 
         });
-    }
-
-    // Add hover on abbreviated planned route for each route
-    for (var j = 0; j < search_result_context.trips.length; j++) {
-        var idx = search_result_context.trips[j]["index"];
-        var full_route_div = $("#full_route_"+idx);
-        var abbr_route_div = $("#abbr_route_"+idx);
-        full_route_div.offset({left: full_route_div.offset().left, top: abbr_route_div.offset().top});
-        full_route_div.toggle();
-        abbr_route_div.mouseenter(function(e) {
-            e.preventDefault();
-            e.stopPropagation();
-            var full_route_div_id = this.id.replace("abbr", "full");
-            $("#"+full_route_div_id).fadeIn(500);
-        });
-        full_route_div.mouseleave(function(e) {
-            e.preventDefault();
-            e.stopPropagation();
-            var full_route_div_id = this.id.replace("abbr", "full");
-            $("#"+full_route_div_id).fadeOut(500);            
-        });        
-    }
-
-
+    } 
 });
